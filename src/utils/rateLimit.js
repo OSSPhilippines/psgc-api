4e21e970e28549f8253e899037deb1f8e3998d6f
Justin Balaguer <justinanthony.primary@gmail.com>
2020-11-28 23:20:27 +0800

    📦 v1.2

--- src/utils/rateLimit.js
+++ src/utils/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const apiLimit = rateLimit({
     windowMs: 24 * 60 * 60 * 1000, // 24 hours
-    max: 50, // limit of each IP
+    max: 100, // limit of each IP
     message: "Uh oh! You have reached the maximum api call (50 calls per day)",
     headers: true
 });
